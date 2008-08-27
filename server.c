c63a6e5816e75fc86d6b32451802d8c54db72535
Evan Klitzke <evan@yelp.com>
2008-08-27 15:54:36 -0700

    fix bug that was making set fail, switch to recvmsg (from recv)

--- server.c
+++ server.c
@@ -12,24 +12,24 @@
 
 #define COGENT_PORT 22122
 
+#define RECVBUFSZ (1 << 16)
+
+void *global_set;
+void *global_get;
+
 inline void handle_get(cogent_cache *cache, proto_client_get *req)
 {
 	cache_item *item = cache_fetch(cache, req->key);
-	if (item == NULL)
-		printf("GET (key = %s, key_len = %d) -> NULL\n", req->key, req->key_len);
-	else
-		printf("GET -> something\n");
 	g_slice_free1(req->key_len, req->key);
 	g_slice_free(proto_client_get, req);
 }
 
 inline void handle_set(cogent_cache *cache, proto_client_set *req)
 {
-	printf("SET (key = %s, key_len = %d)\n", req->key, req->key_len);
 	cache_store(cache, req->key, req->val, req->val_len);
-	g_slice_free1(req->key_len, req->key);
+	//g_slice_free1(req->key_len, req->key);
 	g_slice_free1(req->val_len, req->val);
-	g_slice_free(proto_client_get, req);
+	g_slice_free(proto_client_set, req);
 }
 
 
@@ -50,12 +50,22 @@ int main(int argc, char **argv)
 		exit(EXIT_FAILURE);
 	}
 
-	void *recv_buf = malloc(1 << 16);
+	void *recv_buf = g_slice_alloc(RECVBUFSZ);
+
+	struct msghdr msg;
+	struct iovec iov = {recv_buf, RECVBUFSZ};
+	msg.msg_name = NULL;
+	msg.msg_namelen = 0;
+	msg.msg_iov = &iov;
+	msg.msg_iovlen = 1;
+	msg.msg_control = NULL;
+	msg.msg_controllen = 0;
+	msg.msg_flags = 0;
 
 	cogent_cache *cache = cache_init(1e8);
 
 	while (1) {
-		size_t sz = recv(sock, recv_buf, 1 << 16, 0);
+		size_t sz = recvmsg(sock, &msg, 0);
 		void *s = parse_buffer(recv_buf, sz);
 
 		uint8_t cmd_byte = ((uint8_t *) s)[0];
