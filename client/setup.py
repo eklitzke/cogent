9c64a3bcc4a9ff16df3f894318d26294636e6018
Evan Klitzke <evan@yelp.com>
2008-08-31 12:24:59 -0700

    super hackish way to make setuptools.py jive with pkg-config

--- client/setup.py
+++ client/setup.py
@@ -1,17 +1,32 @@
 #!/usr/bin/python
 
 from distutils.core import setup, Extension
+import commands
 import os
 
 __version__ = '0.1'
 
 macros = [('MODULE_VERSION', '"%s"' % __version__)]
 
+status, output = commands.getstatusoutput('pkg-config --cflags-only-I glib-2.0')
+if status != 0:
+	raise ValueException, 'glib-2.0 not found'
+include_dirs = ['..'] + [x[2:] for x in output.split(' ')]
+
+status, output = commands.getstatusoutput('pkg-config --libs-only-l glib-2.0')
+libraries = [y for y in [x[2:] for x in output.split(' ')] if y]
+
+print 'libraries', libraries
+
+status, output = commands.getstatusoutput('pkg-config --libs-only-L glib-2.0')
+library_dirs = [x[2:] for x in output.split(' ')]
+
 cogent_extension = Extension(
 	name='cogent',
 	sources=['cogent.c'],
-	libraries=['glib-2.0'],
-	include_dirs=["/usr/include/glib-2.0", "/usr/lib/glib-2.0/include", ".."],
+	libraries=libraries,
+	include_dirs=include_dirs,
+	library_dirs=library_dirs,
 	define_macros=macros,
 	extra_objects=['../protocol.o']
 )
